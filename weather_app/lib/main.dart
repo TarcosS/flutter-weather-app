8b8d3cd7232b71e312c54a3c620f9adc23586680
TarcosS <kokkiliculas@gmail.com>
2024-01-12 19:09:40 +0300

    Initialize Weather App

---
+++ weather_app/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFF7EA9E9),
+                Color(0xFF131C6C),
+              ],
+            )
+        ),
+        child: Text("Home"),
+      ),
+    );
+  }
+}
+
